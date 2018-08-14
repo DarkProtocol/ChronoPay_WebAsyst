fb00025876ac20aed01e92c03989b8fdc6be96eb
darkprotocol <svechnick@mail.ru>
2018-08-14 13:00:30 +0300

    fix tabs

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -1,11 +1,11 @@
 
 <?php
 return array(
-	'name'        => 'ChronoPay Payment',
-	'description' => 'Платежная система ChronoPay',
-	'icon'        => 'img/chronopay-icon.jpg',
-	'logo'        => 'img/chronopay.jpg',
-	'version'     => '1.0.0',
-	'locale'      => array('ru_RU',),
-	'type'        => waPayment::TYPE_ONLINE,
+    'name'        => 'ChronoPay Payment',
+    'description' => 'Платежная система ChronoPay',
+    'icon'        => 'img/chronopay-icon.jpg',
+    'logo'        => 'img/chronopay.jpg',
+    'version'     => '1.0.0',
+    'locale'      => array('ru_RU',),
+    'type'        => waPayment::TYPE_ONLINE,
 );
